f47b6b78a89dc6eb31284f3378b84b0af29b7744
holychowders <theholychowders@gmail.com>
2023-04-27 13:46:45 -0500

    Add sample addresses.

---
+++ sql/populate_sample_data/address.sql
@@ -0,0 +1,43 @@
+INSERT INTO address (line1, line2, city, state, zip)
+VALUES (
+    '1313 Disneyland Dr.',
+    '#55',
+    'Anaheim',
+    'California',
+    '92802'
+),
+(
+    '400 Collins Rd NE',
+    '',
+    'Cedar Rapids',
+    'Iowa',
+    '52498'
+),
+(
+    '1201 S 2nd St',
+    '',
+    'Milwaukee',
+    'Wisconsin',
+    '53204'
+),
+(
+    '100 Universal City Plaza',
+    '#7',
+    'Universal City',
+    'California',
+    '91608'
+),
+(
+    '9243 Lakewood Blvd',
+    '',
+    'Downey',
+    'California',
+    '90240'
+),
+(
+    '2941 Fairview Park Dr',
+    '#100',
+    'Falls Church',
+    'Virginia',
+    '22042'
+);
