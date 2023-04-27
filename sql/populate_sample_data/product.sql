c5a34a5de8e35125f0b68380d8abbedd8751ed5e
holychowders <theholychowders@gmail.com>
2023-04-27 13:24:36 -0500

    Merge separate inserts into single command.

--- sql/populate_sample_data/product.sql
+++ sql/populate_sample_data/product.sql
@@ -4,34 +4,26 @@ VALUES (
     'Bran of raisin',
     50,
     6.0
-);
-
-INSERT INTO product (name, description, quantity_available, price)
-VALUES (
+),
+(
     'Raisin Bran Family Size',
     'Big bran of raisin',
     20,
     8.0
-);
-
-INSERT INTO product (name, description, quantity_available, price)
-VALUES (
+),
+(
     'Corn Flakes',
     'Flakes of corn',
     30,
     5.0
-);
-
-INSERT INTO product (name, description, quantity_available, price)
-VALUES (
+),
+(
     'Corn',
     'Head of corn',
     45,
     3.0
-);
-
-INSERT INTO product (name, description, quantity_available, price)
-VALUES (
+),
+(
     'Toothpaste',
     'Paste of tooth',
     20,
