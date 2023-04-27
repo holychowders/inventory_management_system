41c4679c75523c8a15df4c67ef6182ccf4e8fff7
holychowders <theholychowders@gmail.com>
2023-04-26 22:43:23 -0500

    Populate database using SQL scripts in `sql/populate_sample_data/`.
    Added a population script for `product` table.

---
+++ sql/populate_sample_data/product.sql
@@ -0,0 +1,39 @@
+INSERT INTO product (name, description, quantity_available, price)
+VALUES (
+    'Raisin Bran',
+    'Bran of raisin',
+    50,
+    6.0
+);
+
+INSERT INTO product (name, description, quantity_available, price)
+VALUES (
+    'Raisin Bran Family Size',
+    'Big bran of raisin',
+    20,
+    8.0
+);
+
+INSERT INTO product (name, description, quantity_available, price)
+VALUES (
+    'Corn Flakes',
+    'Flakes of corn',
+    30,
+    5.0
+);
+
+INSERT INTO product (name, description, quantity_available, price)
+VALUES (
+    'Corn',
+    'Head of corn',
+    45,
+    3.0
+);
+
+INSERT INTO product (name, description, quantity_available, price)
+VALUES (
+    'Toothpaste',
+    'Paste of tooth',
+    20,
+    3.0
+);
--- src/main.py
+++ src/main.py
@@ -16,10 +16,20 @@ def main() -> None:
         logging.warning("Database file not found. Creating %s", DB_PATH)
         # Connecting creates the database file if it doesn't exist.
         with sqlite3.connect(DB_PATH) as db:
+            logging.info("Database file created: %s", DB_PATH)
             with open(SQL_PATH / "create_tables.sql", encoding="utf-8") as script:
                 db.executescript(script.read())
                 db.commit()
-                logging.info("Database file created: %s", DB_PATH)
+                logging.info("Created tables")
+
+            logging.info("Populating database")
+            population_script_paths = SQL_PATH.glob("populate_sample_data/*")
+            for script_path in population_script_paths:
+                logging.info("Populating database with %s", script_path)
+                with open(script_path, encoding="utf-8") as script:
+                    db.executescript(script.read())
+                    db.commit()
+            logging.info("Populated database")
 
 
 if __name__ == "__main__":
