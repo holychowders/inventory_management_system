bbbae53a6d10c24a2ac4d71e7b02b97105493621
holychowders <theholychowders@gmail.com>
2023-04-27 18:42:53 -0500

    Add customer, purchase, and supplier sample population SQL.

---
+++ sql/populate_sample_data/customer.sql
@@ -0,0 +1,37 @@
+INSERT INTO customer (first_name, last_name, address_id, phone)
+VALUES (
+    'Ferris',
+    'Bueller',
+    1,
+    '1112223333'
+),
+(
+    'Sloane',
+    'Peterson',
+    2,
+    '2223334444'
+),
+(
+    'Phil',
+    'Connors',
+    3,
+    '3334445555'
+),
+(
+    'Rita',
+    'Hanson',
+    3,
+    '4445556666'
+),
+(
+    'Clark',
+    'Griswold',
+    4,
+    '5556667777'
+),
+(
+    'Ellen',
+    'Griswold',
+    4,
+    '5556667777'
+)
---
+++ sql/populate_sample_data/purchase.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO purchase (
+    product_id, customer_id, quantity_purchased, date_purchased, status
+)
+VALUES (1, 3, 10, '2023-01-01', 'SUCCESSFUL'),
+(3, 2, 2, '2023-01-01', 'SUCCESSFUL'),
+(3, 4, 2, '2023-01-02', 'SUCCESSFUL'),
+(5, 3, 4, '2023-01-02', 'DECLINED');
