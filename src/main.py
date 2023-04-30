d265587ec68030f92c334d8db0738f9e6cfb387f
holychowders <theholychowders@gmail.com>
2023-04-29 20:50:29 -0500

    Add basic products page.
    It populates with products from DB and can delete items. Everything else
    is just dummy stuff.

--- src/main.py
+++ src/main.py
@@ -1,28 +1,41 @@
 import logging
-from pprint import pformat
 
-from flask import Flask, render_template
+from flask import Flask, Response, redirect, render_template
 
-from db import all_products, ensure_db
+import db
+from db import Product, ensure_db
 
 flask_app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
 
 def main() -> None:
     ensure_db()
-    logging.info("Products:\n%s", pformat(all_products()))
-
     flask_app.run(host="0.0.0.0", port=80)
 
 
-@flask_app.route("/")  # noqa: vulture
+@flask_app.route("/")
 def index() -> str:
     return render_template("index.html")
 
 
-@flask_app.route("/dashboard")  # noqa: vulture
+@flask_app.route("/dashboard")
 def dashboard() -> str:
     return render_template("dashboard.html")
+
+
+@flask_app.route("/products")
+def products() -> str:
+    products = (Product(product) for product in db.all_products())  # pylint: disable=redefined-outer-name
+    return render_template("products.html", products=products)
+
+
+@flask_app.route("/delete-product/<int:id>")
+def delete_product(id: int) -> Response:  # pylint: disable=redefined-builtin
+    db.delete_product(id)
+
+    # FIXME: This assumes that we are on the products page to begin with (refreshes).
+    # Otherwise, we probably shouldn't be redirecting to it.
+    return redirect("/products")  # type: ignore
 
 
 if __name__ == "__main__":
